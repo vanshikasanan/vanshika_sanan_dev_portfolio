0e82395f6f7a093e756fdbd46270d39b4bda3691
Raghav Goel <raghav@vudio.ai>
2025-07-15 01:56:11 +0530

    Add resume on portflio

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -6,20 +6,32 @@ const Navbar = () => {
   return (
     <div className="navbar">
       {/* Sidebar */}
-      <Sidebar/>
+      <Sidebar />
       <div className="wrapper">
         <motion.span
           initial={{ opacity: 0, scale: 0.5 }}
           animate={{ opacity: 1, scale: 1 }}
           transition={{ duration: 0.5 }}
-        >
-      
-        </motion.span>
+        ></motion.span>
         <div className="social">
+          <motion.button
+            className="download-btn"
+            initial={{ opacity: 0, scale: 0.5 }}
+            animate={{ opacity: 1, scale: 1 }}
+            transition={{ duration: 0.5 }}
+            onClick={() => {
+              const link = document.createElement("a");
+              link.href = "/VanshikaSananResume.pdf";
+              link.download = "VanshikaSananResume.pdf";
+              link.click();
+            }}
+          >
+            Download Resume
+          </motion.button>
+
           <a href="https://www.linkedin.com/in/vanshika-sanan-21a68b177/">
             <img src="/LinkedinLogo.png" alt="" />
           </a>
-        
         </div>
       </div>
     </div>
